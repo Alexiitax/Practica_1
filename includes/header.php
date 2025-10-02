a128a54602bc67c0fed432fcb4868c4a103cc3f7
Valery <valeryliste.loscardones@gmail.com>
2025-10-02 15:55:37 +0000

    valery

---
+++ includes/header.php
@@ -0,0 +1,7 @@
+<header>
+    <h1>Enciclopedia de Animales</h1>
+    <nav>
+        <a href="index.php">Inicio</a>
+        <a href="register-animal.php">Registrar Nuevo Animal</a>
+    </nav>
+</header>
