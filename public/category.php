7ee805621cf6eae9963e8a93ccd589eb13ac9f7e
Alexiitax <alexiita.ull@gmail.com>
2025-10-02 17:47:38 +0000

    Alexa

--- public/category.php
+++ public/category.php
@@ -1,6 +1,32 @@
 <?php
     require_once "../data/datos.php";
+
+    $id = $_GET['id'];
+
+    foreach ($categorias as $cat) {
+        if ($cat['id'] == $id) {
+            $categorias = $cat;
+            break;
+        }
+    }
+
     include "../includes/header.php"; 
 ?>
+
+    <h2><?php echo $categorias['nombre']; ?></h2>
+    <p><?php echo $categorias['descripcion']; ?></p>
+
+    <?php foreach ($animales as $id => $animal): ?>
+    <?php if ($animal['categoria_id'] == $categorias['id']): ?>
+    
+    <li>
+        <a href="animal.php?id=<?php echo $id; ?>">
+                <?php echo $animal['nombre']; ?>
+        </a>
+    </li>
+     
+    <?php endif; ?>
+    <?php endforeach; ?>
+
 
 <?php include "../includes/footer.php"; ?>
