c0eab4f364bf971e1be557a94ddb7679e4b6588d
Valery <valeryliste.loscardones@gmail.com>
2025-10-02 16:07:39 +0000

    valery

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,25 @@
-<header>
-    <h1>Enciclopedia de Animales</h1>
-    <nav>
-        <a href="index.php">Inicio</a>
-        <a href="register-animal.php">Registrar Nuevo Animal</a>
-    </nav>
-</header>
+<?php
+if (!isset($titulo_pagina)){
+    $titulo_pagina = "Enciclopedia de Animales";
+}
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+        echo "<title>$titulo_pagina</title>";
+    ?>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    <header>
+        <h1>Enciclopedia de Animales</h1>
+        <nav>
+            <a href="index.php">Inicio</a>
+            <a href="register-animal.php">Registrar Nuevo Animal</a>
+        </nav>
+    </header>
+
+    <main class="container">
