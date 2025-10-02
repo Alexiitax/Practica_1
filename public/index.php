657efa11fab300363db854aa7cecb80794b3a802
Valery <valeryliste.loscardones@gmail.com>
2025-10-02 17:19:41 +0000

    valery

--- public/index.php
+++ public/index.php
@@ -1,7 +1,13 @@
 <?php
 
     include "../includes/header.php";
-    require_once "../datas/datos.php";
+    require_once "../data/datos.php";
+    echo "<ul>";
+    for($j = 1; $j <= count($categorias); $j++){
+        echo "<li><a href='category.php?id=" . $categorias[$j]["id"] . "'>";
+        echo $categorias[$j]["nombre"] . "</a></li>";
+    }
+    echo "</ul>";
 
 ?>
 
