0bdda2e9835b3cc74f4bccdb82f1fdc226be0811
Alexiitax <alexiita.ull@gmail.com>
2025-10-02 15:56:02 +0000

    Alexa

--- includes/footer.php
+++ includes/footer.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="estilos.css">
-</head>
-<body>
-    <footer>
-
-    </header>
-    
-</body>
-</html>
+<footer>
+    <p>&copy; 2025 - Ejercicio de Desarrollo Web con PHP</p>
+</footer>
