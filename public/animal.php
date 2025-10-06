bae8d3feefe27947cc1e5cb390cedd9a487038e9
Alexa Bozhidarova Gotseva Raneva <alexiita.ull@gmail.com>
2025-10-06 05:12:56 +0100

    alexa
    alexa

--- public/animal.php
+++ public/animal.php
@@ -1,35 +1,36 @@
 <?php
     require_once "../data/datos.php";
-
+    
     $id = $_GET['id'];
-
-    foreach ($animales as $a) {
-        if ($a['id'] == $id) {
-            $animal = $a;
-            break;
-        }
-    }
-
-    if (!$animal) {
+    
+    if (!$id || !isset($animales[$id])) {
         die("Animal no encontrado.");
     }
-
+    
+    $animal = $animales[$id];
     $titulo_pagina = $animal['nombre'];
+
     include "../includes/header.php";
 ?>
 
     <h2><?php echo $animal['nombre']; ?></h2>
     <p><strong>Hábitat:</strong> <?php echo $animal['habitat']; ?></p>
-    <img src="../<?php echo $animal['imagen']; ?>" alt="<?php echo $animal['nombre']; ?>" width="300">
+
+    <img src="../uploads/images/<?php echo $animal['imagen']; ?>" alt="<?php echo $animal['nombre']; ?>" width="300">
 
     <h3>Características:</h3>
     <ul>
-        <?php foreach ($animal['caracteristicas'] as $carac): ?>
-            <li><?php echo $carac; ?></li>
-        <?php endforeach; ?>
+    <?php foreach ($animal['caracteristicas'] as $caracteristica): ?>
+        <li><?php echo $caracteristica; ?></li>
+    <?php endforeach; ?>
     </ul>
 
-    <p><a href="../<?php echo $animal['pdf']; ?>" target="_blank">Ver descripción en PDF</a></p>
-    <p><a href="update-animal.php?id=<?php echo $animal['id']; ?>">Actualizar animal</a></p>
+    <p>
+        <a href="../uploads/pdfs/<?php echo $animal['pdf']; ?>" target="_blank">Ver descripción en PDF</a>
+    </p>
+
+    <p>
+        <a href="update-animal.php?id=<?php echo $id; ?>">Actualizar animal</a>
+    </p>
 
 <?php include "../includes/footer.php"; ?>
